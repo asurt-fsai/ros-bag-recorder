4852f381ddaa2cc20e9b7b13cbb89a2013a7309b
Roni Emad <90726457+RoniEmad@users.noreply.github.com>
2023-06-27 21:03:09 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source /opt/ros/noetic/setup.bash
-source ~/catkin_fs/devel/setup.bash
-bash -c "python3 ~/ros-bag-recorder/run.py"
+bash -c "python3 "$SCRIPT_DIR"/run.py"
 
 exec $SHELL
