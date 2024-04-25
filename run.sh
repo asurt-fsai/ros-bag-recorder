23c4422653d0ddbe184e64412b2a437f592bf2cc
Mohamed Alaa <mohammed.alaa200080@gmail.com>
2024-04-25 13:50:29 +0200

    fix: generalize all ros2 versions

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source /opt/ros/humble/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 bash -c "python3 "$SCRIPT_DIR"/run.py"
 
 exec $SHELL
